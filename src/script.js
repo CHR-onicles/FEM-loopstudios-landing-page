c1f8ea60f2ef6c2ad9f5b77cf492bddfe9cc0605
CHR-onicles <tpandivine48@gmail.com>
2021-08-09 18:44:29 +0000

    done with mobile main header

---
+++ src/script.js
@@ -0,0 +1,12 @@
+const nav_toggle = document.querySelector(".nav-toggle");
+const body = document.getElementById("body");
+const nav_header = document.querySelector(".nav-header");
+const links_container = document.querySelector(".nav-links-container");
+
+nav_toggle.addEventListener("click", () => {
+    body.classList.toggle("no-scroll");
+    nav_header.classList.toggle("show-links");
+    links_container.classList.toggle("show-links");
+    nav_toggle.classList.toggle("show-links");
+    window.scrollTo(0, 0); // to make menu fully cover screen
+})
