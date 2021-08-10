e6e3b87c2224bf5b61da4e7d2a8ede359f8574c3
CHR-onicles <tpandivine48@gmail.com>
2021-08-10 11:54:58 +0000

    making nav menu more responsive

--- src/script.js
+++ src/script.js
@@ -9,4 +9,8 @@ nav_toggle.addEventListener("click", () => {
     links_container.classList.toggle("show-links");
     nav_toggle.classList.toggle("show-links");
     window.scrollTo(0, 0); // to make menu fully cover screen
+    const nav_links = document.querySelector(".nav-links");
+    nav_links_height = nav_links.getBoundingClientRect().height;
+    nav_links.style.top =  `calc(50vh - ${nav_links_height / 2}px)`;
+    console.log(nav_links.style.top);
 });
